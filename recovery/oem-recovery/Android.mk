6e215f4c542bce514cdddd5a946e1fd1bbee7ca4
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-11-06 20:14:33 +0100

    rename librecovery_updater_msm8998 -> librecovery_updater_msm

--- recovery/oem-recovery/Android.mk
+++ recovery/oem-recovery/Android.mk
@@ -1,7 +1,7 @@
 ifeq ($(AB_OTA_UPDATER), true)
 BUILD_OEM_UPDATER := true
 endif
-ifneq ($(filter librecovery_updater_msm8998,$(TARGET_RECOVERY_UPDATER_LIBS)),)
+ifneq ($(filter librecovery_updater_msm,$(TARGET_RECOVERY_UPDATER_LIBS)),)
 BUILD_OEM_UPDATER := true
 endif
 ifeq ($(BUILD_OEM_UPDATER), true)
@@ -13,7 +13,7 @@ LOCAL_C_INCLUDES := bootable/recovery \
 		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_SRC_FILES := gpt-utils.cpp dec.cpp oem-updater.cpp
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-LOCAL_MODULE := librecovery_updater_msm8998
+LOCAL_MODULE := librecovery_updater_msm
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -24,7 +24,7 @@ LOCAL_C_INCLUDES := bootable/recovery \
 LOCAL_SRC_FILES := gpt-utils.cpp
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_SHARED_LIBRARIES += liblog libsparse libcutils
-LOCAL_MODULE := librecovery_updater_msm8998
+LOCAL_MODULE := librecovery_updater_msm
 LOCAL_COPY_HEADERS_TO := gpt-utils/inc
 LOCAL_COPY_HEADERS := gpt-utils.h
 include $(BUILD_SHARED_LIBRARY)
